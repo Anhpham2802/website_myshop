9719f327efa7b1592bb05c273ac3fe92ad2d00c2
anhcherry2802 <anhcherry2802@gmail.com>
2024-01-30 10:13:29 +0700

    username

--- myshop_frontend/src/component/layouts/header.jsx
+++ myshop_frontend/src/component/layouts/header.jsx
@@ -8,7 +8,12 @@ import { SlSocialTwitter } from "react-icons/sl";
 import { BsTelephone } from "react-icons/bs";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { client } from "../../api";
+
 import { Link } from "react-router-dom";
+
+import { DownOutlined } from '@ant-design/icons';
+import { Dropdown } from 'antd';
+import { Space } from 'antd';
 
 const Header = () => {
 
@@ -20,12 +25,21 @@ const Header = () => {
         });
     }
 
+    const items = [
+        {
+            key: '1',
+            label: (
+                <p className=''>Logout</p>
+            ),
+        },
+    ]
+
     return (
         <>
             <div className="w-11/12 h-[500] m-auto">
                 <div className="border-b-[1px] border-b-slate-200">
                     <div className="w-11/12 m-auto pb-0.5 flex flex-row">
-                        <Link to="/" className=" w-[10%] float-start mr-5 my-auto"><img src={myshop} alt=""/></Link>
+                        <Link to="/" className=" w-[10%] float-start mr-5 my-auto"><img src={myshop} alt="" /></Link>
                         {/* <a href="/" className=" w-[10%] float-start mr-5 my-auto"><img src={myshop} alt=""/></a> */}
                         <div className="w-full h-12 flex flex-row justify-between">
                             <div className="flex flex-row">
@@ -50,6 +64,17 @@ const Header = () => {
                                 </div>
                                 <FaRegCircleUser className="my-[15px] ml-4" />
                                 <Link to="/login" className="m-2.5" onClick={getUserInfo}>Login</Link>
+
+                                {/* sau khi dang nhap */}
+                                <Link to="/profile" className="m-2.5">username</Link>
+                                <Dropdown menu={{ items, }} trigger={['click']} className='my-auto border-[1px] rounded-full w-[25px] h-[25px] pl-1.5'>
+                                    <a onClick={(e) => e.preventDefault()}>
+                                        <Space>
+                                            <DownOutlined className='w-[10px]' />
+                                        </Space>
+                                    </a>
+                                </Dropdown>
+
                             </div>
                         </div>
                     </div>
