e021c1e9d877b1004e1e3ebf5e11e1d22e22ac2d
anhcherry2802 <anhcherry2802@gmail.com>
2024-02-01 01:39:02 +0700

    sửa chi tiết sp

--- myshop_frontend/src/pages/users/products/product_detail.jsx
+++ myshop_frontend/src/pages/users/products/product_detail.jsx
@@ -154,9 +154,6 @@ const ProductDetail = () => {
                                 <div className="product-content-right-bottom-content-title-item chitiet">
                                     <p>Chi tiết sản phẩm</p>
                                 </div>
-                                <div className="product-content-right-bottom-content-title-item baoquan">
-                                    <p>Cách bảo quản</p>
-                                </div>
                             </div>
                             <div className="product-content-right-bottom-content">
                                 <div className="product-content-right-bottom-content-chitiet">
@@ -173,103 +170,6 @@ const ProductDetail = () => {
                     </div>
                 </div>
             </div>
-            
-            {/* <div className="product-container">
-            <div className="product-sidebar w-1/5">
-                    <h2>Danh mục</h2>
-                    <ul>
-                        <li>Thời trang nam</li>
-                        <li>Thời trang nữ</li>
-                        <li>Thời trang trẻ em</li>
-                        <li class="active"><a href="#">Trang phục cho nữ</a> </li>
-                        <li><a href="#">Trang phục cho nam</a></li>
-                        <li><a href="#">Trang phục trẻ em</a></li>
-
-                    </ul>
-                    <h2 className="product-filter">Phụ kiện</h2>
-                    <ul>
-                        <li><a href="#">Túi xách - Balo</a></li>
-                        <li><a href="#">Giày dép</a></li>
-                        <li><a href="#">Phụ kiện thời trang</a></li>
-                    </ul>
-                </div>
-                <div className="w-4/5 product">
-                    <div className="product-content">
-                        <div className="w-1/2 mx-auto product-content-left">
-                            <div className="product-content-left-big-img">
-                                <img src={anh1} alt="" className='h-[500px]' />
-                            </div>
-                            <div className="flex product-content-left-small-img">
-                                <img src={nam2} alt="" />
-                                <img src={anh1} alt="" />
-                                <img src={anh1} alt="" />
-                            </div>
-                        </div>
-                        <div className="product-content-right">
-                            <div className="product-content-right-name">
-                                <div className='flex'>
-                                    <h1>ten san pham</h1>
-                                    <CiHeart className='w-1/3 mt-2 font-bold text-2xl' />
-                                </div>
-                                <p>MSP : 123456</p>
-                            </div>
-                            <div className='flex mt-2'>
-                                <div className='font-bold'>
-                                    <p>Giá</p>
-                                    <p className='mt-3'>Màu sắc</p>
-                                    <p className='mt-3'>Chất liệu</p>
-                                    <p className='mt-3'>Kích thước</p>
-                                    <p className='my-3'>Thương hiệu</p>
-                                </div>
-                                <div className='ml-5'>
-                                    <p>: 100.000<sup>đ</sup></p>
-                                    <p className='mt-3'>: xanh</p>
-                                    <p className='mt-3'>: Vari cotton</p>
-                                    <p className='mt-3'>: S</p>
-                                    <p className='my-3'>: Dior</p>
-                                </div>
-                            </div>
-
-                            <div className="product-quantity">
-                                <p className='font-bold'>Số lượng : </p>
-                                <div className='ml-8 -mt-2.5'><Counter /></div>
-                            </div>
-                            <div className="mt-10 text-center product-content-right-button">
-                                <a href="cart.html">THÊM GIỎ HÀNG</a>
-                            </div>
-                            <div className="mt-10 text-center product-content-right-button">
-                                <p className='text-red-700'>Hết hàng</p>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className="product-content-right-bottom">
-                        <div className="product-content-right-bottom-top">
-                        </div>
-                        <div className="product-content-right-bottom-content-big">
-                            <div className="product-content-right-bottom-content-title">
-                                <div className="product-content-right-bottom-content-title-item chitiet">
-                                    <p>Chi tiết sản phẩm</p>
-                                </div>
-                                <div className="product-content-right-bottom-content-title-item baoquan">
-                                    <p>Cách bảo quản</p>
-                                </div>
-                            </div>
-                            <div className="product-content-right-bottom-content">
-                                <div className="product-content-right-bottom-content-chitiet">
-                                    <p>Tránh ánh nắng trực tiếp </p>
-                                </div>
-                                <div className="product-content-right-bottom-content-baoquan">
-                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                                        Impedit perferendis in eligendi laboriosam eaque explicabo.
-                                        cupiditate nihil ipsum non odit facilis corporis accusamus.
-                                        praesentium repudiandae deleniti eum dolore eius aliquam.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
         </div>
     );
 };
